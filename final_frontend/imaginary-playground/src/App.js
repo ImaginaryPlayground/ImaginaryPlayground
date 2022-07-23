a82fce37e5684a796da00419eda2566fc98c9353
HomePC <jimdac@naver.com>
2022-07-23 14:01:02 +0900

    �FE/feature/signup | 로그인 및 회원가입 페이지 구현 80%  완료 | 0723

--- final_frontend/imaginary-playground/src/App.js
+++ final_frontend/imaginary-playground/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </BrowserRouter>
     </div>
