bb7f0f4c0a2131fbfaef75bdf2854e3b554703c1
devyoseph <josephdev123@naver.com>
2022-07-29 11:59:14 +0900

    [🦉BE/S07P12D204-141] JWT토큰발행/Security/OAuth_yys | 0729

--- backend/imagnaryPlayground/src/main/java/com/yodel/imaginaryPlayground/ImagnaryPlaygroundApplication.java
+++ backend/imagnaryPlayground/src/main/java/com/yodel/imaginaryPlayground/ImagnaryPlaygroundApplication.java
@@ -2,6 +2,10 @@ package com.yodel.imaginaryPlayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ImagnaryPlaygroundApplication {
@@ -10,4 +14,13 @@ public class ImagnaryPlaygroundApplication {
 		SpringApplication.run(ImagnaryPlaygroundApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
