9f2eb6f17df30b05465087e3710af79389d59fe1
sr <hmr971127@gmail.com>
2022-08-02 11:19:33 +0900

    [🐨BE/S07P12D204-260]회원기능 작성 중_ksr | 0802

--- backend/imagnaryPlayground/src/main/java/com/yodel/imaginaryPlayground/mapper/UserMapper.java
+++ backend/imagnaryPlayground/src/main/java/com/yodel/imaginaryPlayground/mapper/UserMapper.java
@@ -10,13 +10,14 @@ import java.util.Map;
 
 @Mapper
 public interface UserMapper {
-    int saveUser(UserDto user) throws SQLException;
-    int updateUserInfo(UserDto user) throws SQLException;
-    int deleteUser(int userId) throws SQLException;
+    int saveUser(UserDto user);
+    int updateUserInfo(String username) throws SQLException;
     UserDto detailUser(int userId) throws SQLException;
+    int deleteUser(int userId) throws SQLException;
     // 사용자 전체 검색은 사용하지 않을 것 같아서 제외
-    int countByEmail(String email) throws SQLException;
-    UserDto findByEmail(String email) throws SQLException;
+    int countByEmail(String email);
+    UserDto findByEmail(String email);
+    String saveFile(String document);
     int saveEmailAuth(Map<String, String> map);
     int authEmailCode(EmailCodeVO emailCodeVO);
     int deleteEmailCode(String email);
