405ae01cd3fe103d814e9541aca7c1a581cf679d
권성호 <maystar8956@naver.com>
2022-08-04 23:55:55 +0900

    [👿FE/S07P12D204-338]파이썬 node.js 로드_ksh |0804

--- FE_practice/iot_front/app.js
+++ FE_practice/iot_front/app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 
-const app = express()
+const app = express();
 
-app.get('', (req, res) => {
+app.use('/public', express.static(__dirname + "/public")); 
+
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/about', (req, res) => {
-    res.send('<h1>This is About Page</h1>')
-})
-
 app.listen(3000, () => {
- console.log('I love you 3000')
+ console.log('I call 3000')
 })
