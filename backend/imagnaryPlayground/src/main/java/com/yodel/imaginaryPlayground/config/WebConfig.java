bbe89a6e7e64cfe530aa2244e65f40b8ce898877
devyoseph <josephdev123@naver.com>
2022-08-04 11:25:35 +0900

    [🦉BE/S07P12D204-310] [BE]React와 Oauth 작업_yys | 0804

--- backend/imagnaryPlayground/src/main/java/com/yodel/imaginaryPlayground/config/WebConfig.java
+++ backend/imagnaryPlayground/src/main/java/com/yodel/imaginaryPlayground/config/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration //Cors 세팅을 위한 인터셉터
+@Configuration //Cors 세팅을 위한 인터셉터입니다.
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
