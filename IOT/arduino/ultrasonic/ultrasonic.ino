b8e76b27af74e4df1de9dd5ad2b006a9e85b6e89
임영선 <dudtjs972@gmail.com>
2022-08-05 16:50:43 +0900

    [🦝IOT/S07P12D204-114]초음파 센서 테스트_iys | 0805

--- IOT/arduino/ultrasonic/ultrasonic.ino
+++ IOT/arduino/ultrasonic/ultrasonic.ino
@@ -38,24 +38,28 @@ long duration, distance; // Duration used to calculate distance
  
 void setup()
 {
-Serial.begin (9600);
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
+    Serial.begin (9600);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
  
 void loop()
 {
-/* The following trigPin/echoPin cycle is used to determine the
-distance of the nearest object by bouncing soundwaves off of it. */
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-duration = pulseIn(echoPin, HIGH);
-//Calculate the distance (in cm) based on the speed of sound.
-distance = duration/58.2;
-Serial.println(distance);
-//Delay 50ms before next reading.
-delay(200);
+    /* The following trigPin/echoPin cycle is used to determine the
+    distance of the nearest object by bouncing soundwaves off of it. */
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    Serial.print("duration : ");
+    Serial.println(duration);
+
+    //Calculate the distance (in cm) based on the speed of sound.
+    distance = duration/58.2;
+    Serial.print("distance : ");
+    Serial.println(distance);
+    //Delay 50ms before next reading.
+    delay(400);
 }
