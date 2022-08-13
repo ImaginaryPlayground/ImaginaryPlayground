834369bfa3584ee05b7d8ceebfea794dddfed52e
devyoseph <josephdev123@naver.com>
2022-08-13 13:16:06 +0900

    [🦉BE/S07P12D204-341] [BE]서버세팅 마무리_yys | 0813

--- backend/src/main/java/com/yodel/imaginaryPlayground/config/WebConfig.java
+++ backend/src/main/java/com/yodel/imaginaryPlayground/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:80","https://localhost:80",
-                        "http://localhost:3000", "https://localhost:3000")
+                        "http://localhost:3000", "https://localhost:3000", "https://i7d204.p.ssafy.io")
                 .allowedMethods("*");
     }
 }
