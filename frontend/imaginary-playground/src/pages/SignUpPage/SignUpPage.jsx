5acd71bca9c77ce9b85f80f1d38bc0e86ccb54dd
devyoseph <josephdev123@naver.com>
2022-08-17 20:05:53 +0900

    [💘FE/S07P12D204-357] sns 로그인 이미지 구현 98%_yjh | 0817

--- frontend/imaginary-playground/src/pages/SignUpPage/SignUpPage.jsx
+++ frontend/imaginary-playground/src/pages/SignUpPage/SignUpPage.jsx
@@ -39,6 +39,8 @@ const ColorButton = styled(Button)(({ theme }) => ({
   },
 }));
 
+const imgUrl = process.env.PUBLIC_URL || ""
+
 //SignUpPage 시작
 const SignUpPage = () => {
   // 이메일 유효성 정규식
@@ -356,10 +358,12 @@ const SignUpPage = () => {
           <h5>sns로 간편 회원가입</h5>
         </Grid>
         <Grid item className="sns_signup_btn">
-          <img src="/iconFolder/SnsLogin/kakao_talk.png" alt="카카오톡" />
-          <img src="/iconFolder/SnsLogin/free_icon_google.png" alt="구글" />
+        <img src={`${imgUrl}/iconFolder/SnsLogin/kakao-talk.png`} alt="카카오톡" />
+          
+          <img src={`${imgUrl}/iconFolder/SnsLogin/free-icon-google.png`}
+ alt="구글" />
           <img
-            src="/iconFolder/SnsLogin/naver_icon_1.png"
+            src={`${imgUrl}/iconFolder/SnsLogin/naver_icon_1.png`}
             width="64px"
             height="64px"
             alt="네이버"
