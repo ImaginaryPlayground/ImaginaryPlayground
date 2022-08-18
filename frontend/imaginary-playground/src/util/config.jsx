55b40919677cb602e85dd244932cc93ad0077a98
devyoseph <josephdev123@naver.com>
2022-08-18 12:22:18 +0900

    [🦉FE/S07P12D204-357] 이미지 경로 수정_yys | 0818

--- frontend/imaginary-playground/src/util/config.jsx
+++ frontend/imaginary-playground/src/util/config.jsx
@@ -1,4 +1,4 @@
 export const config = {
-  //api: "https://i7d204.p.ssafy.io/api",
-  api: "http://localhost:8443/api",
+  api: "https://i7d204.p.ssafy.io/api",
+  //api: "http://localhost:8443/api",
 };
