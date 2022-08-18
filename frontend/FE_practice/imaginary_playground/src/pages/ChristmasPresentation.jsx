c99b162c8189372ac497da88295798e46422addd
jimdac3464 <jimdac3464@gmail.com>
2022-08-18 16:02:20 +0900

    [💘FE/S07P12D204-606] 발표전 최종 코드 최적화 95%_yjh | 0818

--- frontend/FE_practice/imaginary_playground/src/pages/ChristmasPresentation.jsx
+++ frontend/FE_practice/imaginary_playground/src/pages/ChristmasPresentation.jsx
@@ -7,6 +7,7 @@ import { Howl, Howler } from "howler";
 import { config } from "../util/config";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { io } from "socket.io-client";
 
 const ChristmasPresentation = () => {
   const [kidsData, setKidsData] = useState([]);
@@ -45,6 +46,8 @@ const ChristmasPresentation = () => {
             setKidsData(res.data.data);
           } else {
             console.log("데이터를 가져오는 데서 오류가 발생했습니다.!");
+            alert("다시 로그인 해주세요!");
+            navigate("/login");
           }
         })
         .catch((err) => {
@@ -54,19 +57,105 @@ const ChristmasPresentation = () => {
     }
   }, []);
 
+  useEffect(() => {
+    const socket = io("http://192.168.100.221:3001");
+    setTimeout(() => {
+      socket.emit("faceOn");
+      socket.on("face", (info) => {
+        let nowPerson = [];
+
+        for (let key in info) {
+          switch (info[key]) {
+            case "ksh":
+              nowPerson.push("권성호");
+              break;
+            case "iys":
+              nowPerson.push("임영선");
+              break;
+            case "psj":
+              nowPerson.push("박소정");
+              break;
+            case "yys":
+              nowPerson.push("양요셉");
+              break;
+            case "ksr":
+              nowPerson.push("김성령");
+              break;
+            case "yjh":
+              nowPerson.push("유지홍");
+              break;
+            default:
+              break;
+          }
+        }
+
+        console.log(nowPerson);
+        setNowRecognitionPerson(nowPerson.join(", "));
+        const kidBoxes = document.getElementsByClassName("kid_box");
+
+        for (let index = 0; index < kidBoxes.length; index++) {
+          const kid_box = kidBoxes[index];
+          kid_box.classList.remove("scale_different");
+          kid_box.classList.remove("bg_aqua");
+          kid_box.classList.remove("bg_violet");
+          kid_box.classList.remove("bg_green");
+          kid_box.classList.remove("bg_yello");
+          kid_box.classList.remove("bg_orange");
+          kid_box.classList.remove("bg_crimson");
+
+          if (nowPerson.includes(kid_box.childNodes[1].innerText)) {
+            kid_box.classList.add("scale_different");
+
+            switch (kid_box.classList[0]) {
+              case "kid_box_1":
+                kid_box.classList.add("bg_crimson");
+                break;
+              case "kid_box_2":
+                kid_box.classList.add("bg_aqua");
+
+                break;
+              case "kid_box_3":
+                kid_box.classList.add("bg_yello");
+
+                break;
+              case "kid_box_4":
+                kid_box.classList.add("bg_orange");
+
+                break;
+              case "kid_box_5":
+                kid_box.classList.add("bg_green");
+
+                break;
+              case "kid_box_6":
+                kid_box.classList.add("bg_violet");
+
+                break;
+              default:
+                break;
+            }
+          }
+        }
+      });
+    }, 1000);
+
+    return () => {
+      socket.emit("faceOff");
+    };
+  }, []);
+
   return (
     <div className="christmas ChristmasPresentation ">
       <>
-        <div className="kid_box_1 animate__animated animate__tada">
+        <div className="kid_box_1 animate__animated animate__tada kid_box">
           <div className="img_box ">
             <img src={`https://${kidsData[0]?.profile}`} alt="" />
           </div>
           <div style={{ textAlign: "end", marginBottom: "10px" }}>
             {kidsData[0]?.name}
           </div>
-          <div className="kid_text">{kidsData[0]?.character}</div>
+          <div className="kid_text ">{kidsData[0]?.character}</div>
         </div>
-        <div className="kid_box_2 animate__animated animate__tada">
+        <div className="kid_box_2 animate__animated animate__tada kid_box">
           <div className="img_box">
             <img src={`https://${kidsData[1]?.profile}`} alt="" />
           </div>
@@ -75,7 +164,7 @@ const ChristmasPresentation = () => {
           </div>
           <div className="kid_text">{kidsData[1]?.character}</div>
         </div>
-        <div className="kid_box_3 animate__animated animate__tada">
+        <div className="kid_box_3 animate__animated animate__tada kid_box">
           <div className="img_box">
             <img src={`https://${kidsData[2]?.profile}`} alt="" />
           </div>
@@ -84,7 +173,7 @@ const ChristmasPresentation = () => {
           </div>
           <div className="kid_text">{kidsData[2]?.character}</div>
         </div>
-        <div className="kid_box_4 animate__animated animate__tada">
+        <div className="kid_box_4 animate__animated animate__tada kid_box bg_black">
           <div className="img_box">
             <img src={`https://${kidsData[3]?.profile}`} alt="" />
           </div>
@@ -93,7 +182,7 @@ const ChristmasPresentation = () => {
           </div>
           <div className="kid_text">{kidsData[3]?.character}</div>
         </div>
-        <div className="kid_box_5 animate__animated animate__tada">
+        <div className="kid_box_5 animate__animated animate__tada kid_box">
           <div className="img_box">
             <img src={`https://${kidsData[4]?.profile}`} alt="" />
           </div>
@@ -102,7 +191,7 @@ const ChristmasPresentation = () => {
           </div>
           <div className="kid_text">{kidsData[4]?.character}</div>
         </div>
-        <div className="kid_box_6 animate__animated animate__tada">
+        <div className="kid_box_6 animate__animated animate__tada kid_box">
           <div className="img_box">
             <img src={`https://${kidsData[5]?.profile}`} alt="" />
           </div>
@@ -127,7 +216,12 @@ const ChristmasPresentation = () => {
 
       <div className="now_recognition_text">
         현재 인식된 사람:{" "}
-        <span className="now_recognition_name">{nowRecognitionPerson}</span>
+        <span
+          className="now_recognition_name"
+          style={nowRecognitionPerson.length ? { padding: "20px" } : {}}
+        >
+          {nowRecognitionPerson}
+        </span>
       </div>
 
       {/* <Tree id={'tree'}></Tree> */}
@@ -143,7 +237,7 @@ const ChristmasPresentation = () => {
         className="christmas-background"
       />
       {/* 배경 */}
-      <div class="snow"></div>
+      <div className="snow"></div>
       <img
         src="/assets/map/minimap.png"
         alt=""
